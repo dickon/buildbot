7d827c9bf806c42f7e6c53a6373e147232f27c3a
Dickon Reed <dickon@cantab.net>
2011-06-07 22:15:33 +0100

    refactor: use sequencer to simpliy describe_tag

--- master/buildbot/changes/multigit.py
+++ master/buildbot/changes/multigit.py
@@ -219,9 +219,8 @@ def describe_tag(tag_format, format_data, index, repositories, offset=-1):
     format data and the previous tag on this branch, across repositories"""
     tag = tag_format % dict(format_data, index=index)
     prev = tag_format % dict(format_data, index=index+offset)
-    def do_log(gitd):
-        return lambda: git(gitd, 'log', prev+'..'+tag)
-    deferred = sequencer([do_log(gitd) for gitd in repositories])
+    deferred = sequencer(repositories, callback=git, 
+                         arguments=['log', prev+'..'+tag])
     def annotate(textlist):
         return 'Differences between %s and %s:\n%s' % (
             prev, tag, reduce(str.__add__, textlist))
